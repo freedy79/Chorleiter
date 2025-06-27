fc3b507a07e1c95bc4a03ebeb37f7374bf0f4176
freedy79 <michael.free@gmx.de>
2025-06-27 11:00:52 +0200

    Allow admins full choir management rights

--- choir-app-frontend/src/app/features/choir-management/manage-choir-resolver.ts
+++ choir-app-frontend/src/app/features/choir-management/manage-choir-resolver.ts
@@ -4,6 +4,7 @@ import { Observable, forkJoin, of } from 'rxjs';
 import { catchError, switchMap } from 'rxjs/operators';
 import { ApiService } from 'src/app/core/services/api.service';
 import { ErrorService } from 'src/app/core/services/error.service';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,29 +13,41 @@ export class ManageChoirResolver implements Resolve<any> {
   constructor(
     private apiService: ApiService,
     private errorService: ErrorService,
-    private router: Router
+    private router: Router,
+    private authService: AuthService
   ) {}
 
   resolve(): Observable<any> {
     // Leeren Sie alte Fehler, bevor Sie eine neue Seite laden
     this.errorService.clearError();
 
-    return this.apiService.checkChoirAdminStatus().pipe(
-      switchMap(status => {
+    return this.authService.isAdmin$.pipe(
+      switchMap(isAdmin => {
         const choirDetails$ = this.apiService.getMyChoirDetails();
-        if (status.isChoirAdmin) {
+        if (isAdmin) {
           return forkJoin({
             choirDetails: choirDetails$,
             members: this.apiService.getChoirMembers(),
             isChoirAdmin: of(true)
           });
-        } else {
-          return forkJoin({
-            choirDetails: choirDetails$,
-            members: of([]),
-            isChoirAdmin: of(false)
-          });
         }
+        return this.apiService.checkChoirAdminStatus().pipe(
+          switchMap(status => {
+            if (status.isChoirAdmin) {
+              return forkJoin({
+                choirDetails: choirDetails$,
+                members: this.apiService.getChoirMembers(),
+                isChoirAdmin: of(true)
+              });
+            } else {
+              return forkJoin({
+                choirDetails: choirDetails$,
+                members: of([]),
+                isChoirAdmin: of(false)
+              });
+            }
+          })
+        );
       }),
       catchError((error) => {
         const errorMessage = error.error?.message || 'Could not load data for choir management.';
