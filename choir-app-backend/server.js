d0add1eb26e5b50352b56211486c861f13e60fa8
michael.free <michael.free@sartorius.com>
2025-07-01 22:00:48 +0200

    try catch.... in backend server

--- choir-app-backend/server.js
+++ choir-app-backend/server.js
@@ -20,11 +20,11 @@ const demoSeed = async () => {
             }
         });
 
-        await demoUser.addChoir(choir).catch(() => {});
+        await demoUser.addChoir(choir).catch(() => { });
 
         const collection = await db.collection.findByPk(1);
         if (collection) {
-            await choir.addCollection(collection).catch(() => {});
+            await choir.addCollection(collection).catch(() => { });
         }
     } catch (err) {
         console.error("Error during demo seeding:", err);
@@ -81,30 +81,32 @@ const initialSeed = async () => {
 const PORT = process.env.PORT || 8088;
 const ADDRESS = process.env.ADDRESS || "localhost"
 
+console.log("Database synchronized.");
+
 // In development, you might want to force-sync the DB
 // db.sequelize.sync({ force: true }).then(() => {
 //   console.log("Drop and re-sync db.");
 // });
 db.sequelize.sync({ alter: true })
     .then(() => {
-        console.log("Database synchronized.");
 
-        // Rufen Sie die Seed-Funktion auf.
-        initialSeed();
-        demoSeed();
+        try {
+            console.log("Database synchronized.");
 
-    const server = app.listen(PORT, ADDRESS, () => {
-        console.log(`Server is running on port ${PORT}, listening ${ADDRESS}.`);
-    });
-    // Close requests that take longer than 20 seconds
-    server.setTimeout(20 * 1000);
-    server.on('timeout', (socket) => {
-        console.warn('Request timed out.');
-        socket.destroy();
-    });
+            // Rufen Sie die Seed-Funktion auf.
+            initialSeed();
+            demoSeed();
+
+            const server = app.listen(PORT, ADDRESS, () => {
+                console.log(`Server is running on port ${PORT}, listening ${ADDRESS}.`);
+            });
+            // Close requests that take longer than 20 seconds
+            server.setTimeout(20 * 1000);
+            server.on('timeout', (socket) => {
+                console.warn('Request timed out.');
+                socket.destroy();
+            });
+        } catch (err) {
+            console.error("Database startup failed:", err);
+        }
 });
-
-   /*.catch((err) => {
-        console.error("Database synchronization failed:", err);
-    });*/
-
