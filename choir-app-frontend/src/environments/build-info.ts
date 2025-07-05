78fc134eaf811d3d95d796ba37f69736531f05a6
Michael Free <michael.free@gmx.de>
2025-07-05 13:59:44 +0200

    new version

--- choir-app-frontend/src/environments/build-info.ts
+++ choir-app-frontend/src/environments/build-info.ts
@@ -1,5 +1,5 @@
 export const buildInfo = {
-  version: '0.1.0',
-  commit: 'e02c042',
-  date: '2025-07-02T11:37:20.969Z'
+  version: '0.0.0',
+  commit: 'a64f142',
+  date: '2025-07-05T11:58:39.596Z'
 };
