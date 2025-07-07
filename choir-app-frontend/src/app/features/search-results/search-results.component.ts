0661ac35b6ba9c08f4cb9a2cf287d7e6dcf47d47
freedy79 <michael.free@gmx.de>
2025-07-07 22:04:23 +0200

    feat: add search results page

---
+++ choir-app-frontend/src/app/features/search-results/search-results.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { MaterialModule } from '@modules/material.module';
+import { ApiService } from '@core/services/api.service';
+import { Piece } from '@core/models/piece';
+import { Event } from '@core/models/event';
+import { Collection } from '@core/models/collection';
+
+@Component({
+  selector: 'app-search-results',
+  standalone: true,
+  imports: [CommonModule, MaterialModule, RouterModule],
+  templateUrl: './search-results.component.html',
+  styleUrls: ['./search-results.component.scss']
+})
+export class SearchResultsComponent implements OnInit {
+  query = '';
+  results: { pieces: Piece[]; events: Event[]; collections: Collection[] } = { pieces: [], events: [], collections: [] };
+
+  constructor(private route: ActivatedRoute, private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe(params => {
+      this.query = params.get('q') || '';
+      if (this.query) {
+        this.api.searchAll(this.query).subscribe(res => this.results = res);
+      } else {
+        this.results = { pieces: [], events: [], collections: [] };
+      }
+    });
+  }
+}
--- choir-app-frontend/src/app/shared/components/search-box/search-box.component.spec.ts
+++ choir-app-frontend/src/app/shared/components/search-box/search-box.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
 import { of } from 'rxjs';
 import { ApiService } from '@core/services/api.service';
 import { SearchBoxComponent } from './search-box.component';
@@ -10,12 +11,17 @@ describe('SearchBoxComponent', () => {
   let fixture: ComponentFixture<SearchBoxComponent>;
 
   let apiSpy: jasmine.SpyObj<ApiService>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(async () => {
     apiSpy = jasmine.createSpyObj('ApiService', ['searchAll']);
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
     await TestBed.configureTestingModule({
       imports: [SearchBoxComponent, HttpClientTestingModule],
-      providers: [{ provide: ApiService, useValue: apiSpy }]
+      providers: [
+        { provide: ApiService, useValue: apiSpy },
+        { provide: Router, useValue: routerSpy }
+      ]
     }).compileComponents();
 
     apiSpy.searchAll.and.returnValue(of({ pieces: [], events: [], collections: [] }));
@@ -36,4 +42,10 @@ describe('SearchBoxComponent', () => {
     expect(apiSpy.searchAll).toHaveBeenCalledWith('abc');
 
   }));
+
+  it('should navigate to results', () => {
+    component.searchCtrl.setValue('term');
+    component.goToResults();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/search'], { queryParams: { q: 'term' } });
+  });
 });
