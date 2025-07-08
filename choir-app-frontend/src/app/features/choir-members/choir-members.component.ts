bae4974c14feea0a8c1463d048dec03b7c7c1d2a
freedy79 <michael.free@gmx.de>
2025-07-08 21:41:55 +0200

    Add choir member list component

---
+++ choir-app-frontend/src/app/features/choir-members/choir-members.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableDataSource } from '@angular/material/table';
+import { MaterialModule } from '@modules/material.module';
+import { ApiService } from '@core/services/api.service';
+import { UserInChoir } from '@core/models/user';
+
+@Component({
+  selector: 'app-choir-members',
+  standalone: true,
+  imports: [CommonModule, MaterialModule],
+  templateUrl: './choir-members.component.html',
+  styleUrls: ['./choir-members.component.scss']
+})
+export class ChoirMembersComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'voice', 'email', 'street', 'postalCode', 'city'];
+  dataSource = new MatTableDataSource<UserInChoir>();
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getChoirMembers().subscribe(members => {
+      this.dataSource.data = members;
+    });
+  }
+
+  mask(value: string | undefined, share: boolean | undefined): string {
+    return share ? (value || '') : '*****';
+  }
+}
