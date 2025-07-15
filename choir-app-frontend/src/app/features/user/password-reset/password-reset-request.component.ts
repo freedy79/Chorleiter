b383396d7a5ad2502a0d397065e7bac2ffea4c2a
Michael Free <michael.free@gmx.de>
2025-07-15 13:56:11 +0200

    updated layout

--- choir-app-frontend/src/app/features/user/password-reset/password-reset-request.component.ts
+++ choir-app-frontend/src/app/features/user/password-reset/password-reset-request.component.ts
@@ -27,11 +27,11 @@ export class PasswordResetRequestComponent {
     this.isLoading = true;
     this.api.requestPasswordReset(this.form.value.email || '').subscribe({
       next: () => {
-        this.message = 'Wenn du registriert bist, erhältst du eine E-Mail mit einem Link zum Erstellen eines neuen Passworts.';
+        this.message = 'Wenn du registriert bist, erhältst du eine E-Mail mit einem Link zum Erstellen eines neuen Passworts. <br> Bitte beachte, dass diese Mail eventuell in dem Spamordner deines Emailproviders landen kann. Falls du innerhalb der nächsten 5 Minuten keine Mail erhältst, schaue bitte dort nach.';
         this.isLoading = false;
       },
       error: () => {
-        this.message = 'Wenn du registriert bist, erhältst du eine E-Mail mit einem Link zum Erstellen eines neuen Passworts.';
+        this.message = 'Wenn du registriert bist, erhältst du eine E-Mail mit einem Link zum Erstellen eines neuen Passworts. <br> Bitte beachte, dass diese Mail eventuell in dem Spamordner deines Emailproviders landen kann. Falls du innerhalb der nächsten 5 Minuten keine Mail erhältst, schaue bitte dort nach.';
         this.isLoading = false;
       }
     });
