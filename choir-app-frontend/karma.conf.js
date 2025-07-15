8c24d11d3b1d15653b6f4333d69f34163c86d38e
freedy79 <michael.free@gmx.de>
2025-07-15 19:25:09 +0200

    test: bundle puppeteer for karma

--- choir-app-frontend/karma.conf.js
+++ choir-app-frontend/karma.conf.js
@@ -1,3 +1,4 @@
+process.env.CHROME_BIN = process.env.CHROME_BIN || require('puppeteer').executablePath();
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -27,7 +28,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
