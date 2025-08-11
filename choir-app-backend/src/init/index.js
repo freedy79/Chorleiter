fcc326650dfd7a95cf9e97a5ea890d44c74cd84f
freedy79 <michael.free@gmx.de>
2025-08-11 19:26:40 +0200

    Migrate role column to roles array

--- choir-app-backend/src/init/index.js
+++ choir-app-backend/src/init/index.js
@@ -1,9 +1,11 @@
 const { syncDatabase } = require('./dbSync');
 const { ensureJoinHashes } = require('./joinHashes');
 const { seedDatabase } = require('../seed');
+const { migrateRoles } = require('./migrateRoles');
 
 async function init(options = {}) {
     const { includeDemoData = true, syncOptions = { alter: true } } = options;
+    await migrateRoles();
     await syncDatabase(syncOptions);
     await ensureJoinHashes();
     await seedDatabase({ includeDemoData });
@@ -13,4 +15,5 @@ module.exports = {
     init,
     syncDatabase,
     ensureJoinHashes,
+    migrateRoles,
 };
