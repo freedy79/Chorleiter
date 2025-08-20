618f2555c4426638a6aa81652cc42af29647771b
freedy79 <michael.free@gmx.de>
2025-08-20 22:50:55 +0200

    Synchronize available choirs with current user

--- choir-app-frontend/src/app/layout/choir-switcher/choir-switcher.component.ts
+++ choir-app-frontend/src/app/layout/choir-switcher/choir-switcher.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { MaterialModule } from '@modules/material.module';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { Choir } from 'src/app/core/models/choir';
@@ -17,11 +18,16 @@ export class ChoirSwitcherComponent {
   activeChoir$: Observable<Choir | null>;
 
   constructor(private authService: AuthService) {
-    this.availableChoirs$ = this.authService.availableChoirs$;
-    this.activeChoir$ = this.authService.activeChoir$;
+    this.availableChoirs$ = this.authService.availableChoirs$.pipe(
+      tap(choirs => console.debug('ChoirSwitcher available choirs', choirs))
+    );
+    this.activeChoir$ = this.authService.activeChoir$.pipe(
+      tap(choir => console.debug('ChoirSwitcher active choir', choir))
+    );
   }
 
   onChoirChange(choirId: number): void {
+    console.debug('ChoirSwitcher onChoirChange', choirId);
     this.authService.switchChoir(choirId).subscribe();
   }
 }
