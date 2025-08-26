2b76625decb39249fba71639e8e765145fcd2606
freedy79 <michael.free@gmx.de>
2025-08-26 14:34:56 +0200

    feat(program): allow adding free pieces

---
+++ choir-app-frontend/src/app/features/program/program-free-piece-dialog.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MaterialModule } from '@modules/material.module';
+
+@Component({
+  selector: 'app-program-free-piece-dialog',
+  standalone: true,
+  imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './program-free-piece-dialog.component.html',
+  styleUrls: ['./program-free-piece-dialog.component.scss'],
+})
+export class ProgramFreePieceDialogComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder, private dialogRef: MatDialogRef<ProgramFreePieceDialogComponent>) {
+    this.form = this.fb.group({
+      title: ['', Validators.required],
+      composer: [''],
+      instrument: [''],
+      performerNames: [''],
+      duration: ['', Validators.pattern(/^\d{1,2}:\d{2}$/)],
+    });
+  }
+
+  save() {
+    if (this.form.valid) {
+      const { duration, ...rest } = this.form.value;
+      let durationSec: number | undefined;
+      if (duration) {
+        const [m, s] = duration.split(':').map((v: string) => parseInt(v, 10));
+        durationSec = m * 60 + s;
+      }
+      this.dialogRef.close({ ...rest, durationSec });
+    }
+  }
+
+  cancel() {
+    this.dialogRef.close();
+  }
+}
