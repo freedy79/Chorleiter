994538eb4d5d616357d625be3e8c60cee9f453fc
freedy79 <michael.free@gmx.de>
2025-09-09 08:53:14 +0200

    Update reset token on sending password reset mail

--- choir-app-backend/src/controllers/password-reset.controller.js
+++ choir-app-backend/src/controllers/password-reset.controller.js
@@ -20,8 +20,8 @@ exports.requestPasswordReset = async (req, res) => {
     if (user) {
       const token = crypto.randomBytes(32).toString('hex');
       const expiry = new Date(Date.now() + 60 * 60 * 1000); // 1 hour
-      await user.update({ resetToken: token, resetTokenExpiry: expiry });
       await emailService.sendPasswordResetMail(user.email, token, user.name, user.firstName);
+      await user.update({ resetToken: token, resetTokenExpiry: expiry });
     }
     res.status(200).send({ message: 'If registered, you will receive an email with a reset link.' });
   } catch (err) {
