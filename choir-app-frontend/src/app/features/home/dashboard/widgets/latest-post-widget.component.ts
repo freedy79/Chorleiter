0f66e072ab2bc23c42d4fcf3859b70cb3a2c3e87
Michael Free <michael.free@gmx.de>
2025-09-12 22:48:48 +0200

    updated Post widget

--- choir-app-frontend/src/app/features/home/dashboard/widgets/latest-post-widget.component.ts
+++ choir-app-frontend/src/app/features/home/dashboard/widgets/latest-post-widget.component.ts
@@ -1,17 +1,25 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MaterialModule } from '@modules/material.module';
 import { Post } from '@core/models/post';
+import { MarkdownPipe } from '@shared/pipes/markdown.pipe';
 
 @Component({
   selector: 'app-latest-post-widget',
   standalone: true,
-  imports: [CommonModule, RouterModule, MaterialModule],
+  imports: [CommonModule, RouterModule, MaterialModule, MarkdownPipe],
   templateUrl: './latest-post-widget.component.html',
   styleUrls: ['./latest-post-widget.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LatestPostWidgetComponent {
-  @Input() post: Post | null = null;
+   @Input() post: Post | null = null;
+
+   constructor(private router: Router) {}
+
+   openLatestPost(post: Post): void {
+    this.router.navigate(['/posts'], { fragment: `post-${post.id}` });
+  }
+
 }
